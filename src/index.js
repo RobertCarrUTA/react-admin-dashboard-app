7bd43419f5d04229fd47cf49eb869dd2945ad2cb
Robert Carr <70494103+RobertCarrUTA@users.noreply.github.com>
2022-07-02 18:00:56 -0500

    Updating src folder
    Adding folders and files to the src folder

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom'; // Hook the React application to the real DOM
 
+import './index.css'; // Imports all tailwind utilities
 import App from './App';
 
 // Hook our React application to the root div
