51ea2cd04a2e44b4f6a3694fcf3d68990f681894
Robert Carr <70494103+RobertCarrUTA@users.noreply.github.com>
2022-07-03 15:39:36 -0500

    Added a Few Things to The Homepage
    The homepage now contains an SVG image, as well as an earnings section with a download button. It also includes 4 cards, customers, products, sales, and refunds. Each card displays the title for the card with a corresponding image, it includes the data numbers for the specific card and includes a colored percentage gain or loss for what the card represents. If it is a net gain, the percentage is green, if it is a net loss, the percentage is red.
    
    Everything seems to be working as it should with no errors at this point of the project.

--- src/App.js
+++ src/App.js
@@ -41,9 +41,7 @@ const App = () => {
           }>
             <div className = 'fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
               <Navbar />
-            </div>
-          </div>
-          
+            </div>          
           <div>
             <Routes>
               {/* Dashboard */}
@@ -72,7 +70,7 @@ const App = () => {
               <Route path = "/stacked" element = {<Stacked/>} />
             </Routes>
           </div>
-
+          </div>
         </div>
       </BrowserRouter>
     </div>
