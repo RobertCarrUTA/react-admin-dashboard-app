6a5d4b4ca117b2bd7a9daea0c48f96126ffeea66
Robert Carr <70494103+RobertCarrUTA@users.noreply.github.com>
2022-07-02 20:16:02 -0500

    Added Pages, Updated App.js, and Fixed Issue
    Added the basis for the pages, and updated the App.js file to replace the previous strings with components. Had an issue with my template string on like 38 of App.js, but removing my comment removed the error and everything seems to be working as it should now.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import {FiSettings} from 'react-icons/fi';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
+import { Navbar, Footer, Sidebar, ThemeSettings } from './components';
+import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor, Line } from './pages';
+
 import './App.css';
 
 const App = () => {
@@ -11,10 +14,10 @@ const App = () => {
   return (
     <div>
       <BrowserRouter>
-        <div classname = "flex relative dark:bg-main-dark-bg">
-          <div classname = "fixed right-4 bottom-4" style = {{ zindex: '1000'}}>
+        <div className = "flex relative dark:bg-main-dark-bg">
+          <div className = "fixed right-4 bottom-4" style = {{ zindex: '1000'}}>
             <TooltipComponent content= "Settings" position = "Top">
-              <button type = "button" classname = "text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white"
+              <button type = "button" className = "text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white"
               style = {{ background: 'blue', borderRadius: '50%' }}>
                 <FiSettings />
               </button>
@@ -22,14 +25,14 @@ const App = () => {
           </div>
           {activeMenu ? (
             <div className = "w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">
-              Sidebar
+              <Sidebar />
             </div>
           ) : (
             <div className="w-0 dark:bg-secondary-dark-bg">
-              Sidebar
+              <Sidebar />
             </div>
           )}
-          <div className = { {/* is activeMenu true? */}
+          <div className = {
             // Use of a template string below makes code shorter while achieving the same outcome
             // When things are partially repetitive (shared classes), use a template string like this to shorten the code
             `dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu ? 'md:ml-72' : 'flex-2'}`
@@ -42,28 +45,29 @@ const App = () => {
           <div>
             <Routes>
               {/* Dashboard */}
-              <Route path = "/ecommerce" element = "ECommerce"/>
+              <Route path = "/" element = {<Ecommerce/>} />
+              <Route path = "/ecommerce" element = {<Ecommerce/>} /> {/* Use ALT + Click to edit multiple lines at once  */}
 
               {/* Pages */}
-              <Route path = "/orders" element = "Orders"/>
-              <Route path = "/employees" element = "Employees"/>
-              <Route path = "/customers" element = "Customers"/>
+              <Route path = "/orders" element = {<Orders/>  } />
+              <Route path = "/employees" element = {<Employees/>} />
+              <Route path = "/customers" element = {<Customers/>} />
               
               {/* Apps */}
-              <Route path = "/calendar" element = "Calendar"/>
-              <Route path = "/kanban" element = "Kanban"/>
-              <Route path = "/editor" element = "Editor"/>
-              <Route path = "/color-picker" element = "ColorPicker"/>
+              <Route path = "/calendar" element = {<Calendar/>} />
+              <Route path = "/kanban" element = {<Kanban/>} />
+              <Route path = "/editor" element = {<Editor/>} />
+              <Route path = "/color-picker" element = {<ColorPicker/>} />
 
               {/* Charts */}
-              <Route path = "/line" element = "Line"/>
-              <Route path = "/area" element = "Area"/>
-              <Route path = "/bar" element = "Bar"/>
-              <Route path = "/pie" element = "Pie"/>
-              <Route path = "/financial" element = "Financial"/>
-              <Route path = "/color-mapping" element = "ColorMapping"/>
-              <Route path = "/pyramid" element = "Pyramid"/>
-              <Route path = "/stacked" element = "Stacked"/>
+              <Route path = "/line" element = {<Line/>} />
+              <Route path = "/area" element = {<Area/>} />
+              <Route path = "/bar" element = {<Bar/>} />
+              <Route path = "/pie" element = {<Pie/>} />
+              <Route path = "/financial" element = {<Financial/>} />
+              <Route path = "/color-mapping" element = {<ColorMapping/>} />
+              <Route path = "/pyramid" element = {<Pyramid/>} />
+              <Route path = "/stacked" element = {<Stacked/>} />
             </Routes>
           </div>
 
