584561062082536aabd0b588119fc80cf0febd67
Robert Carr <70494103+RobertCarrUTA@users.noreply.github.com>
2022-07-02 19:20:51 -0500

    Added basis for Components + additions to App.js
    Added the initial starts for all of the project components and added a little more to App.js.

--- src/App.js
+++ src/App.js
@@ -34,8 +34,39 @@ const App = () => {
             // When things are partially repetitive (shared classes), use a template string like this to shorten the code
             `dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu ? 'md:ml-72' : 'flex-2'}`
           }>
-
+            <div className = 'fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
+              Navbar
+            </div>
           </div>
+          
+          <div>
+            <Routes>
+              {/* Dashboard */}
+              <Route path = "/ecommerce" element = "ECommerce"/>
+
+              {/* Pages */}
+              <Route path = "/orders" element = "Orders"/>
+              <Route path = "/employees" element = "Employees"/>
+              <Route path = "/customers" element = "Customers"/>
+              
+              {/* Apps */}
+              <Route path = "/calendar" element = "Calendar"/>
+              <Route path = "/kanban" element = "Kanban"/>
+              <Route path = "/editor" element = "Editor"/>
+              <Route path = "/color-picker" element = "ColorPicker"/>
+
+              {/* Charts */}
+              <Route path = "/line" element = "Line"/>
+              <Route path = "/area" element = "Area"/>
+              <Route path = "/bar" element = "Bar"/>
+              <Route path = "/pie" element = "Pie"/>
+              <Route path = "/financial" element = "Financial"/>
+              <Route path = "/color-mapping" element = "ColorMapping"/>
+              <Route path = "/pyramid" element = "Pyramid"/>
+              <Route path = "/stacked" element = "Stacked"/>
+            </Routes>
+          </div>
+
         </div>
       </BrowserRouter>
     </div>
