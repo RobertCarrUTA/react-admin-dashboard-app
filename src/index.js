5a186f8b4cd31a63d113a33e9788020a0f300727
Robert Carr <70494103+RobertCarrUTA@users.noreply.github.com>
2022-07-03 02:13:42 -0500

    Added the Context API
    Now a user can close the sidebar, but cannot bring it back open, that will be added later.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom'; // Hook the React application to the real DOM
 
 import './index.css'; // Imports all tailwind utilities
 import App from './App';
+import { ContextProvider } from './contexts/ContextProvider';
 
 // Hook our React application to the root div
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <ContextProvider>
+        <App />
+    </ContextProvider>,
+    document.getElementById('root')
+);
