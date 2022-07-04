e785f2feb0bd55a6eb06853c71705ffbbf977ee9
Robert Carr <70494103+RobertCarrUTA@users.noreply.github.com>
2022-07-04 00:16:04 -0500

    At a stopping point for now
    Started working on the settings sidebar with the themes, but I am taking a break for now.

--- src/App.js
+++ src/App.js
@@ -42,6 +42,13 @@ const App = () => {
             <div className = 'fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
               <Navbar />
             </div>          
+
+          {/* Theme Component */}
+          <div>
+            <ThemeSettings />
+          </div>
+
+          {/* Sidebar routes */}
           <div>
             <Routes>
               {/* Dashboard */}
