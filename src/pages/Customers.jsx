918aebaf1578bddf5c710bd2540758cbaba35019
Robert Carr <70494103+RobertCarrUTA@users.noreply.github.com>
2022-07-03 20:09:05 -0500

    Added Orders, Customers, and Employees pages
    The user can now view and use different features to sort, edit, delete, or search the data across the different pages added. Up to this point, the project is working as intended. As far as I can tell, there are no errors.

--- src/pages/Customers.jsx
+++ src/pages/Customers.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from 'react';
+import  { GridComponent, ColumnsDirective, ColumnDirective, Page, Selection, Edit, Search, Inject, Toolbar, Sort, Filter } from '@syncfusion/ej2-react-grids';
+
+import { customersData, customersGrid } from '../data/dummy';
+import { Header } from '../components';
+
 
 const Customers = () => {
   return (
-    <div>Customers</div>
+    <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="Page" title="Customers" />
+      <GridComponent
+        id="gridcomp"
+        dataSource={customersData}
+        allowPaging
+        allowSorting
+        editSettings={{ allowDeleting: true, allowEditing: true }}
+        toolbar={['Delete']}
+        width="auto"
+      >
+        <ColumnsDirective>
+          {customersGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+        {/* The below Inject allows for us to select what page of orders to view
+         The features in the inject are included in the array*/}
+        <Inject services={[Page, Selection, Edit, Search, Toolbar, Sort, Filter]} />
+      </GridComponent>
+    </div>
   )
 }
 
-export default Customers
+export default Customers;
